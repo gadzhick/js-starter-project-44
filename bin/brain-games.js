ff00e062cffd9dfb763b6678c28c287ad83511f9
dmitryg <dmitryg@eljur.team>
2023-03-14 22:04:13 +0300

    rework cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import askName from '../src/cli.js';
+import askQuestion from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-askName();
+const answer = askQuestion('May I have your name? ');
+console.log(`Hello, ${answer}!`)
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
 import readlineSync from 'readline-sync';
 
-export default function askName() {
-  const userName = readlineSync.question('May I have your name? ');
-  const result = `Hello, ${userName}!`;
-  console.log(result);
+export default function askQuestion(question) {
+  return readlineSync.question(question);
 }
