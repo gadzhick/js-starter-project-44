ee5d73e79ec9796bbc973c266b150c22a9e127a9
dmitryg <dmitryg@eljur.team>
2023-03-15 00:00:30 +0300

    add new game: brain-even; add editorconfig for better coding

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,5 @@
 import askQuestion from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-const answer = askQuestion('May I have your name? ');
-console.log(`Hello, ${answer}!`)
+const name = askQuestion('May I have your name? ');
+console.log(`Hello, ${name}!`);
